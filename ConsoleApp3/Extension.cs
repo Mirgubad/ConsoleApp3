97d103510ef7c032611bb78df1a8b9c3f1d13d8c
Mirgubad <migubad@code.edu.az>
2022-06-07 16:58:20 +0400

    Add project files.

---
+++ ConsoleApp3/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public static class Extension
+    {
+
+        public static int[] Add(this int[] arr, int num)
+        {
+            arr=new int[arr.Length+1] ;
+           Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = num;
+
+            return arr;
+        }
+
+    }
+}
